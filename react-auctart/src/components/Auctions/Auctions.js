96385918715153f680b63a01eee5fb53c6bfa83f
Irina <irinakjoseva1@gmail.com>
2019-07-11 21:03:46 +0200

    Working pagination

--- react-auctart/src/components/Auctions/Auctions.js
+++ react-auctart/src/components/Auctions/Auctions.js
@@ -8,19 +8,19 @@ import {withNotificationContext} from "../../context/NotificationContext";
 class Auctions extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {auctions: []}
+        this.state = {auctions: []};
         getActiveAuctions().then(auctions => {
             this.setState({
                 auctions: auctions,
                 currentPage: 1,
                 auctionsPerPage: 3
-            }).catch(() => {
-                this.props.notificationSystem.current.addNotification({
-                    title: "Error",
-                    message: "Something went wrong",
-                    level: "error",
-                    autoDismiss: 3
-                });
+            })
+        }).catch(() => {
+            this.props.notificationSystem.current.addNotification({
+                title: "Error",
+                message: "Something went wrong",
+                level: "error",
+                autoDismiss: 3
             });
         });
     }
@@ -56,17 +56,17 @@ class Auctions extends React.Component {
         );
         children.push(
             <td key={"td2_"+index} className="py-4">
-                <h4>
+                <h3>
                     {auction.name}&nbsp;
-                </h4>
+                </h3>
                 <h3>
                     <sub>
                         by {auction.author}
                     </sub>
                 </h3>
-                <h2>
-                    ${auction.startPrice}
-                </h2>
+                <h5>
+                    Active until: {auction.endDate.slice(0,10)}
+                </h5>
                 <div className="float-to-bottom float-right">
                     <PlaceBid/>
                 </div>
